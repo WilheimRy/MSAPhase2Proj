e7ac46149ef6191606a73f3ebb043dc76636c003
WilheimRy <wilheim.ry@gmail.com>
2016-11-21 22:12:20 +1300

    MainPage

--- FindYourFood/FindYourFood/FindYourFood/App.cs
+++ FindYourFood/FindYourFood/FindYourFood/App.cs
@@ -12,22 +12,9 @@ namespace FindYourFood
         public App()
         {
             // The root page of your application
-            var content = new ContentPage
-            {
-                Title = "FindYourFood",
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            HorizontalTextAlignment = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
+            
 
-            MainPage = new NavigationPage(content);
+            MainPage = new Views.MainPage();
         }
 
         protected override void OnStart()
